a0d51e7aa5e227adff744475bb210b77fe5b1e7d
hughesg703 <granth6@vt.edu>
2018-03-04 14:02:31 -0500

    Test front-end for Basement Labs mobile application

--- App.js
+++ App.js
@@ -1,41 +1,79 @@
+/**
+ * Copyright (c) 2018, Grant Hughes, All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ */
+
 import React, { Component } from 'react';
 import { StyleSheet, AppRegistry, Text, TextInput, View, Button, Alert } from 'react-native';
 
+/**
+ * @file Test Client slide for Basement Labs app. Form with user name input and message input
+ *       to be sent to client database
+ * @author Grant Hughes
+ * @version 0.1
+ */
 export default class Test extends Component {
+
+  /**
+   * Constructor for App.js class. Initializes user and message strings to empty
+   *
+   * @param props - Props for the constructor
+   */
   constructor(props) {
     super(props);
-    this.state = {text: ''};
+    this.state = {user: '', message: ''};
   }
 
+  /**
+  * Process form submit. Sends POST reqest to application API containing
+  * user and message values submitted into form.
+  */
+  submitForm(user, message) {
+
+    if(user == '' && message == '') {
+      Alert.alert("Failure: One or more fields are empty.");
+    }
+    else {
+
+      var url = "http://ec2-34-238-119-166.compute-1.amazonaws.com/message";
+
+      var result = fetch(url, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          'user': user,
+          'message': message
+        })
+      })
+      .then(console.log("hi"))
+      .catch(e => { throw e; });
+      Alert.alert("Success: Your message " + '"' + this.state.message + '"' + " has been entered into the database.");
+    }
+  }
+
+  /**
+  * Function to render React Hyptertext onto screen of mobile device
+  */
   render() {
     return (
-      <View style={{
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#FFFFFF'
-      }}>
+      <View style={styles.container}>
+        <Text style={{fontSize: 20, fontWeight: 'bold', width: 200, textAlign: 'center'}}>Send a message to the database</Text>
         <TextInput
-          style={{
-            height: 60
-          }}
-          placeholder="Enter a message here"
-          onChangeText={(text) => this.setState({text})}
+          style={styles.user}
+          placeholder="Name"
+          onChangeText={(user) => this.setState({user})}
         />
-        <View style={{
-          borderRadius: 10,
-          padding: 3,
-          shadowColor: '#000000',
-          shadowOffset: {
-            width: 0,
-            height: 1
-          },
-          shadowRadius: 5,
-          shadowOpacity: 0.15
-        }}>
+        <TextInput
+          style={styles.message}
+          placeholder="Message"
+          onChangeText={(message) => this.setState({message})}
+        />
+        <View style={styles.button}>
           <Button
             onPress={() => {
-              Alert.alert("Your message " + '"' + this.state.text + '"' + " has been entered into the database");
+              this.submitForm(this.state.user, this.state.message)
             }}
             title="Enter"
           />
@@ -45,6 +83,46 @@ export default class Test extends Component {
   }
 }
 
+// Styles for form components
+const styles = StyleSheet.create({
+  container: {
+    marginTop: 100,
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#FFFFFF'
+  },
+  user: {
+    height: 40,
+    width: 200,
+    backgroundColor: '#f6f6f6',
+    paddingLeft: 10,
+    paddingRight: 10,
+    marginTop: 15,
+    marginBottom: 5
+  },
+  message: {
+    height: 40,
+    width: 200,
+    backgroundColor: '#f6f6f6',
+    paddingLeft: 10,
+    paddingRight: 10,
+    marginTop: 5,
+    marginBottom: 10
+  },
+  button: {
+    borderRadius: 10,
+    padding: 3,
+    shadowColor: '#000000',
+    shadowOffset: {
+      width: 0,
+      height: 1
+    },
+    shadowRadius: 5,
+    shadowOpacity: 0.15,
+    marginTop: 10
+  }
+});
 
 // skip this line if using Create React Native App
 AppRegistry.registerComponent('client', () => Test);
